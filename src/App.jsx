c14a2b50a5eb8a0bcda48d9b971be36d0228898b
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-14 14:14:21 +0200

    Add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import About from './components/About';
 import { CartProvider } from './store/CartContext';
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <CartProvider>
         <Navbar />
         <Hero />
+        <About />
       </CartProvider>
     </>
   );
