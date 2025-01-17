0dad3242096944d29da0b9d70bd9e20f28752ebf
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-01-17 09:48:53 +0200

    Edit Brands sction

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col, Image } from 'react-bootstrap';
+
 import Img1 from '../assets/images/logo-1.png';
 import Img2 from '../assets/images/logo-2.png';
 import Img3 from '../assets/images/logo-3.png';
@@ -7,6 +8,7 @@ import Img5 from '../assets/images/logo-5.png';
 import Img6 from '../assets/images/logo-6.png';
 import Img7 from '../assets/images/logo-7.png';
 import Img8 from '../assets/images/logo-8.png';
+
 export default function Brands() {
   return (
     <section
@@ -14,63 +16,67 @@ export default function Brands() {
       className="pt-3 pb-3 pt-md-4 pb-md-4 pt-lg-5 pb-lg-5"
     >
       <Container>
-        <Row className="gx-lg-5">
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img1}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img2}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img3}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img4}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img5}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img6}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img7}
-              fluid
-            />
-          </Col>
-          <Col>
-            <Image
-              style={{ height: '100%', objectFit: 'contain' }}
-              src={Img8}
-              fluid
-            />
-          </Col>
+        <Row className="row-cols-1 row-cols-md-2 gap-4 gap-md-0 justify-content-between justify-content-evenly">
+          <Row>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img1}
+                fluid
+              />
+            </Col>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img2}
+                fluid
+              />
+            </Col>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img3}
+                fluid
+              />
+            </Col>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img4}
+                fluid
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img5}
+                fluid
+              />
+            </Col>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img6}
+                fluid
+              />
+            </Col>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img7}
+                fluid
+              />
+            </Col>
+            <Col>
+              <Image
+                style={{ height: '100%', objectFit: 'contain' }}
+                src={Img8}
+                fluid
+              />
+            </Col>
+          </Row>
         </Row>
       </Container>
     </section>
